89726bcb50498f78ac88e47b974f0dd04357e57d
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-01-16 15:51:58 +0100

    Adicionando o cors ao projecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:Lutero19@cluster0-zt5sd.mongodb.net/week10?retryWrites=true&w=majority',{
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://omnistack:Lutero19@cluster0-zt5sd.mongodb.net/we
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.listen(3333);
 //Tipos de parametros 
